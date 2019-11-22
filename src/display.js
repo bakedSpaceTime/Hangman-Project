4c587dff7c15767dbdabfa727280c35dd2aa7c6b
bakedSpaceTime <tushyais@gmail.com>
2019-11-22 13:47:30 -0800

    fix bugs with displaying guessed letters

--- src/display.js
+++ src/display.js
@@ -10,31 +10,37 @@ canv.height = height;
 
 canv.style.border = '2px solid blue '
 ctx.font = '35px consolas'
-ctx.fillStyle = 'blue'
-ctx.strokeStyle = 'blue'
+ctx.fillStyle = 'red'
+ctx.fillRect(0,0,width,height)
+
 
 function drawBottomLine(){
+
+    ctx.lineWidth = 5
+    ctx.strokeStyle = 'blue'
+    ctx.beginPath()
     ctx.moveTo(width * 0.9, height * 0.7)
     ctx.lineTo(width * 0.1, height * 0.7)
     ctx.stroke()
 }
 
 function calculateStartWidth(){
-    return width * (0.5 - word.length / 2 * 0.04)
+    return width * (0.5 - word.length / 2 * 0.035)
 }
 
 function displayBlankLines(){
-
-    // clearDisplay()
-    // drawBottomLine()
 
     let w = calculateStartWidth()
        
     ctx.lineWidth = 5
     
-    for(let i = 0; i < word.length; i++){
-        // ctx.fillText(word[i], w + width * 0.04 * i, height * 0.55)
+    ctx.strokeStyle = 'blue'
 
+    for(let i = 0; i < word.length; i++){
+
+        console.log(word.length)
+
+        ctx.beginPath()
         ctx.moveTo(w + i * width * 0.04, height * 0.6)
         ctx.lineTo(w + i * width * 0.04 + ctx.measureText(word[i]).width , height * 0.6)
         ctx.stroke()
@@ -44,14 +50,15 @@ function displayBlankLines(){
 
 function displayLetter(letter, i){
     let w = calculateStartWidth()
+    ctx.fillStyle = 'blue'
     
     ctx.fillText(letter, w + width * 0.04 * i, height * 0.55)
 }
 
 function clearDisplay(){
+    ctx.fillStyle = 'red'
     ctx.clearRect(0, 0, width, height)
+    ctx.fillRect(0,0,width,height)
 
     console.log(width)
 }
-
-
