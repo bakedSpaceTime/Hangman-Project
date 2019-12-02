732b8bfa49aae379760e84b72291018c89d85e52
bakedSpaceTime <tushyais@gmail.com>
2019-12-02 14:28:49 -0800

    add semicolons, remove consol.log, format code

--- src/alphabet.js
+++ src/alphabet.js
@@ -1,48 +1,48 @@
 
 
-function alphabetButtons(n){
+function alphabetButtons(n) {
 
     let btnArr = [];
 
     let i = 0;
-    while(i < n){
+    while (i < n) {
         btnArr.push(new createLetterButton(i));
-        i ++;
-    }    
+        i++;
+    }
 
     return btnArr;
 }
 
-function createLetterButton(dec){
+function createLetterButton(dec) {
     this.node = document.createElement('button');
 
     this.node.style.margin = '5px';
     this.node.style.width = '30px';
     this.node.className = 'buttons';
-    
+
     this.node.appendChild(document.createTextNode(decToAlpha(dec)));
-    
-    this.addToBody = function(){
+
+    this.addToBody = function () {
         document.getElementById('buttonDiv').appendChild(this.node);
     }
-    
-    this.changeID = function() {
-        this.id = 'clicked_button';       
+
+    this.changeID = function () {
+        this.id = 'clicked_button';
     }
-    
-    this.node.addEventListener('click', function(){addLetterToGuess(this)});
+
+    this.node.addEventListener('click', function () { addLetterToGuess(this) });
     this.node.addEventListener('click', this.changeID);
     this.addToBody();
 }
 
-function decToAlpha(code){
+function decToAlpha(code) {
     return String.fromCharCode(code + 65);
 }
 
-function clearButtons(){
+function clearButtons() {
     let butdiv = document.getElementById('buttonDiv');
 
-    while(butdiv.firstChild){
+    while (butdiv.firstChild) {
         butdiv.removeChild(butdiv.firstChild);
     }
 
--- src/gamescript.js
+++ src/gamescript.js
@@ -1,16 +1,16 @@
 let words = [['INTEND', 'have in mind as a purpose'],
-            ['BITCOIN', 'an online payment system that does not require an intermediary'],
-            ['CONSTANT', 'a quantity that does not vary'],
-            ['APPEAL', 'be attractive to'],
-            ['TATTOO', 'a form of body modification where a design is made by inserting ink'],
-            ['LEAGUE', 'an association of sports teams that organize matches'],
-            ['FINANCIAL', 'involving fiscal matters'],
-            ['ELECTRICITY', 'the set of physical phenomena associated with the presence and motion of electric charge.'],
-            ['TERRITORY', 'the geographical area under the jurisdiction of a state'],
-            ['RESOURCE', 'a new or reserve supply that can be drawn upon when needed'],
-            ['ATTRIBUTE', 'a quality belonging to or characteristics of an entity'],
-            ['COMMITTEE', 'a group of people appointed for a specific function']];
-        
+['BITCOIN', 'an online payment system that does not require an intermediary'],
+['CONSTANT', 'a quantity that does not vary'],
+['APPEAL', 'be attractive to'],
+['TATTOO', 'a form of body modification where a design is made by inserting ink'],
+['LEAGUE', 'an association of sports teams that organize matches'],
+['FINANCIAL', 'involving fiscal matters'],
+['ELECTRICITY', 'the set of physical phenomena associated with the presence and motion of electric charge.'],
+['TERRITORY', 'the geographical area under the jurisdiction of a state'],
+['RESOURCE', 'a new or reserve supply that can be drawn upon when needed'],
+['ATTRIBUTE', 'a quality belonging to or characteristics of an entity'],
+['COMMITTEE', 'a group of people appointed for a specific function']];
+
 let word = '';
 let score = 0
 let lives = 7
@@ -19,11 +19,11 @@ let userGuess = '';
 let userGuessList = [];
 
 function playGame() {
-    
+
     word = randomWord();
-    
+
     displayHint(word);
-    
+
     initializeDisplay();
 }
 
@@ -44,10 +44,10 @@ function addLetterToGuess(thisbttn) {
             update_score = false;
         }
     }
-    
+
     thisbttn.changeID;
-    
-    if(update_score){
+
+    if (update_score) {
         changeScore(currentGuess);
     }
 
@@ -66,13 +66,13 @@ function resetGame() {
     lives = 7;
     userGuess = '';
     userGuessList = [];
-    
+
     clearButtons()
     alphabetButtons(26)
     document.getElementById('buttonDiv').style.display = '';
     document.getElementById('score').style.display = '';
     document.getElementById('lives').style.display = '';
-    
+
     clearDisplay();
     setTimeout(playGame, 50);
 }
@@ -80,13 +80,13 @@ function resetGame() {
 function continueGame() {
     userGuess = '';
     userGuessList = [];
-    
+
     clearButtons();
     alphabetButtons(26);
     document.getElementById('buttonDiv').style.display = '';
     document.getElementById('score').style.display = '';
     document.getElementById('lives').style.display = '';
-    
+
     clearDisplay();
     setTimeout(playGame, 50);
 }
@@ -95,11 +95,11 @@ function gameOver() {
     document.getElementById('buttonDiv').style.display = 'None';
     document.getElementById('score').style.display = 'None';
     document.getElementById('lives').style.display = 'None';
-    
+
     let name = prompt('Please enter your name.', 'Random Player');
-    
+
     document.getElementById('hint').innerHTML = "Game over, " + name + "! You lost!<br>Your final score is: " + score;
-    
+
     score = 0;
     lives = 7;
 }
